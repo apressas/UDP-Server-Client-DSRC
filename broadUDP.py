9da160ac420cec9ed415bddece5835eded90a019
Andreas <apressas@users.noreply.github.com>
2020-09-09 02:37:53 +0300

    Update broadUDP.py
    cleanup

--- broadUDP.py
+++ broadUDP.py
@@ -9,7 +9,6 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 # indefinitely when trying to receive data.
 server.settimeout(0.2)
 server.connect(("192.168.10.11", 1235))
-#message = b"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 pcktsize =  int(raw_input("how big is the packet : ") or "200")
 msg = 'X'
 message_r = msg*pcktsize
@@ -25,9 +24,7 @@ i=0
 for i in range (10000):
     send_msg(server,message)
     i=i+1
-    # send_msg(server,message)
-    #print(message)
-    #if i%50==0:
+    
     sys.stdout.write("No of TX: %d   \r" % (i) )
     sys.stdout.flush()
     time.sleep(0.02)
